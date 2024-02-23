fd18c6b5c6766e7bf3d56146760a0cbed6e709c2
Natacha Fernández <hello@natacha.me>
2024-02-23 17:46:59 +0000

    New JS file

---
+++ skills.js
@@ -0,0 +1,10 @@
+function calculateNumber(value1, value2) {
+  return value1 + value2;
+}
+
+function divideNumbers(value1, value2) {
+  if (value2 === 0) {
+    return 'Error';
+  }
+  return value1 / value2;
+}
